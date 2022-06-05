e9ba8d606cb715d4c4a4d78e63bf215d702b840e
Joakim <joakimrasmuson@gmail.com>
2022-06-05 21:22:57 +0200

    Add Hitomezashi square stitch pattern

---
+++ hitomezashi_stitch_pattern_square.js
@@ -0,0 +1,18 @@
+// Hitomezashi stitch pattern, inspired by https://youtu.be/JbfhzlMk2eY
+<script src="https://koda.nu/simple.js">
+  
+  var threshold = 0.5;
+  var n = 50;
+  var size = 500 / n;
+
+  for (var i = 0; i < n; i++) {
+    var shiftX = Math.random() > threshold ? 1.0 : 0.0;
+    var shiftY = Math.random() > threshold ? 1.0 : 0.0;
+    
+    for (var j = 0; j < n / 2; j++) {
+      line(size * (2.0 * j + shiftX), size * i, size * (2.0 * j + 1.0 + shiftX), size * i);
+      line(size * i, size * (2.0 * j + shiftY), size * i, size * (2.0 * j + 1.0 + shiftY));
+    }
+  }
+
+</script>
